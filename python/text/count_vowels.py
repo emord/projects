f06fe5e22bca78ec4ced3433cde747ad9dec101a
Jonathan Emord <jonathan.emord@gmail.com>
2014-02-10 20:11:46 -0500

    Add complex counting of vowels [Python]

--- python/text/count_vowels.py
+++ python/text/count_vowels.py
@@ -9,9 +9,19 @@ def count_vowels_basic(s):
 
     return result
 
+def count_vowels_complex(s):
+    result = {'a':0, 'e':0, 'i':0, 'o':0, 'u':0}
+
+    for letter in s:
+        if letter.lower() in result.keys():
+            result[letter.lower()] += 1
+    
+    return result
+
 def main():
     s = input("Count vowels in this: ")
     print(count_vowels_basic(s))
+    print(count_vowels_complex(s))
 
 if __name__ == '__main__':
     main()
